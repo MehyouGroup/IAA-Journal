8a2402ddc734ef8bb06ca2a2b10a2b251bbf694d
MehyouGroup <it@mehyougroup.com>
2025-09-03 16:47:01 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ source: "IAA Journal (Sep).pdf", //you can change the path and the name of your 
 
     hard: "hard", 
 
-	backgroundImage: "assets/phoenix.png", 
+	backgroundImage: "assets/snowy-winter-landscape.jpg", 
 	backgroundColor: "#fff",  
 
 	soundEnable: true, 
