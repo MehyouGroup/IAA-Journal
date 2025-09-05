54e313416433d58357e4dca8e4237f11b5510248
MehyouGroup <it@mehyougroup.com>
2025-09-05 17:44:41 +0300

    Update toc.js

--- toc.js
+++ toc.js
@@ -1,5 +1,15 @@
 option_PDF1.outline = [
-      {title: "TOC Chapter 1", dest: 1},
-      {title: "TOC Chapter 2", dest: 2},
-      {title: "TOC Chapter 3", dest: 3}
+  { title: "Note from the CEO – Captain Wissam Mehyou", dest: 2 },
+  { title: "Academy News & Milestones", dest: 3 },
+  { title: "Student Spotlight", dest: 8 },
+  { title: "Instructor Insights", dest: 11 },
+  { title: "Flight Science & Innovation", dest: 12 },
+  { title: "Industry Trends & Global Aviation Outlook", dest: 13 },
+  { title: "Featured Article – Building Your Career Path", dest: 14 },
+  { title: "FlyIAA Course – Type Rating Programs", dest: 15 },
+  { title: "Frequently Asked Questions", dest: 17 },
+  { title: "Video Feature", dest: 19 },
+  { title: "Upcoming Events & Programs", dest: 20 },
+  { title: "Resources & Career Opportunities", dest: 21 },
+  { title: "Quote of the Month", dest: 22 }
 ];
