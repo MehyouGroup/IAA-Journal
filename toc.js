047a295fbf3ce2805c733b5ae8f0d9a0d0539bf6
MehyouGroup <it@mehyougroup.com>
2025-09-05 17:48:27 +0300

    Update toc.js

--- toc.js
+++ toc.js
@@ -1,15 +1,15 @@
 option_PDF1.outline = [
-  { title: "Note from the CEO – Captain Wissam Mehyou", dest: 2 },
-  { title: "Academy News & Milestones", dest: 3 },
-  { title: "Student Spotlight", dest: 8 },
-  { title: "Instructor Insights", dest: 11 },
-  { title: "Flight Science & Innovation", dest: 12 },
-  { title: "Industry Trends & Global Aviation Outlook", dest: 13 },
-  { title: "Featured Article – Building Your Career Path", dest: 14 },
-  { title: "FlyIAA Course – Type Rating Programs", dest: 15 },
-  { title: "Frequently Asked Questions", dest: 17 },
-  { title: "Video Feature", dest: 19 },
-  { title: "Upcoming Events & Programs", dest: 20 },
-  { title: "Resources & Career Opportunities", dest: 21 },
-  { title: "Quote of the Month", dest: 22 }
+  { title: "Note from the CEO – Captain Wissam Mehyou", dest: 3 },
+  { title: "Academy News & Milestones", dest: 4 },
+  { title: "Student Spotlight", dest: 9 },
+  { title: "Instructor Insights", dest: 12 },
+  { title: "Flight Science & Innovation", dest: 13 },
+  { title: "Industry Trends & Global Aviation Outlook", dest: 14 },
+  { title: "Featured Article – Building Your Career Path", dest: 15 },
+  { title: "FlyIAA Course – Type Rating Programs", dest: 16 },
+  { title: "Frequently Asked Questions", dest: 18 },
+  { title: "Video Feature", dest: 20 },
+  { title: "Upcoming Events & Programs", dest: 21 },
+  { title: "Resources & Career Opportunities", dest: 22 },
+  { title: "Quote of the Month", dest: 23 }
 ];
